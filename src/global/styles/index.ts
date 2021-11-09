78c6396311d887d17ca6d29bf91800369eb9dcbb
mjsil <m.j.silveira@outlook.com>
2021-11-09 14:19:04 -0300

    arquivos globais

--- src/global/styles/index.ts
+++ src/global/styles/index.ts
@@ -1,9 +1,17 @@
 export const Theme = {
   colors: {
+    primary: '#FFDB01',
 
+    text: '#72727A',
+    text_dark: '#000000',
+
+    background: '#EFEFEF',
+    shape: '#FFFFFF',
   },
 
   fonts: {
-
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
   }
 }
